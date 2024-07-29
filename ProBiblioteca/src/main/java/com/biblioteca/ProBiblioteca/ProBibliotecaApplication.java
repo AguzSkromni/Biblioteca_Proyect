c2ed394e1739cceed444e42c94741763f6b6f685
Agustin Reyes Hernandez <agustin.reyesh95@gmail.com>
2024-07-29 17:57:15 -0600

    Inicio proyecto biblioteca
    Proyecto limpio con el que se inicia la gestion y conexion de la biblioteca, se ira trabajando de a poco para terminar el proyecto. Tendra una landing page que desplegara todos los autores que se tendra en la base de datos, asi como tambien, libros, editoriales, etc...

---
+++ ProBiblioteca/src/main/java/com/biblioteca/ProBiblioteca/ProBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.ProBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProBibliotecaApplication.class, args);
+	}
+
+}
