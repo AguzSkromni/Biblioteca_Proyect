910edfd159c4bc62011ac5eedf44cdf3095f73c8
Agustin Reyes Hernandez <agustin.reyesh95@gmail.com>
2024-08-06 12:50:49 -0600

    Index terminado, autores terminado, trabajando en libros
    See termino el front de la pagina de index, se termino el apartado de autores y se hace un guardado en git, ya que se implementara una nueva actividad que sera realizar filtros mediante controlado, front y logica utilizando forms
    Por lo que se hace este guardado para poder regresar si se comete algun error

--- ProBiblioteca/src/main/java/com/biblioteca/ProBiblioteca/controlador/IndexControlador.java
+++ ProBiblioteca/src/main/java/com/biblioteca/ProBiblioteca/controlador/IndexControlador.java
@@ -1,10 +1,6 @@
 package com.biblioteca.ProBiblioteca.controlador;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 import com.biblioteca.ProBiblioteca.modelo.Autor;
 import com.biblioteca.ProBiblioteca.modelo.Categoria;
@@ -15,6 +11,8 @@ import com.biblioteca.ProBiblioteca.servicio.IEditorialServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collections;
 
 import com.biblioteca.ProBiblioteca.modelo.Libro;
 import com.biblioteca.ProBiblioteca.servicio.ILibroServicio;
@@ -38,6 +36,7 @@ public class IndexControlador {
     public String mostrarIndex(Model model) {
         randomLibroIndex(model);
         categoriaIndex(model);
+        autoresDestacados(model);
 
         return "index.xhtml"; // Retorna el nombre de la vista
     }
@@ -55,6 +54,13 @@ public class IndexControlador {
     @GetMapping("/libros")
     public String listarLibros(Model model) {
         List<Libro> listadoLibros = libroServicio.listarLibros();
+
+        Collections.shuffle(listadoLibros);
+
+        for (Libro libro : listadoLibros) {
+            System.out.println(libro);
+        }
+
         model.addAttribute("libros", listadoLibros);
         return "libros.xhtml";
     }
@@ -86,7 +92,9 @@ public class IndexControlador {
         // Crea 3 valores random en un SET, no permite que haya un valor igual
         while (numerosAleatorios.size() < 3) {
             int numeroAleatorios = random.nextInt(totalLibros);
-            numerosAleatorios.add(numeroAleatorios);
+            if(numeroAleatorios != 0) {
+                numerosAleatorios.add(numeroAleatorios);
+            }
         }
 
         // Pasamos el set a un Array para poderlo leer y manipular
@@ -116,7 +124,9 @@ public class IndexControlador {
         // Crea 3 valores random en un SET, no permite que haya un valor igual
         while (numerosAleatorios.size() < 5) {
             int numeroAleatorios = random.nextInt(totalCategorias);
-            numerosAleatorios.add(numeroAleatorios);
+            if(numeroAleatorios != 0){
+                numerosAleatorios.add(numeroAleatorios);
+            }
         }
 
         // Pasamos el set a un Array para poderlo leer y manipular
@@ -131,6 +141,40 @@ public class IndexControlador {
 
         // Mandando los libros a la vista
         model.addAttribute("categorias", categoriaRandom);
+        return model.toString();
+    }
+
+    public String autoresDestacados(Model model){
+        List<Autor> autoresTodos = autorServicio.listarAutores();
+        // Recuperando el numero total de categorias
+        int totalAutores = autoresTodos.size();
+        // variable Random
+        Random random = new Random();
+        // Genera un numero aleatorio entre el total de categorias obtenidos
+        Set<Integer> numerosAleatorios = new HashSet<>();
+
+        // Crea 3 valores random en un SET, no permite que haya un valor igual
+        while (numerosAleatorios.size() < 3) {
+            int numeroAleatorios = random.nextInt(totalAutores);
+            if(numeroAleatorios != 0){
+                numerosAleatorios.add(numeroAleatorios);
+            }
+
+        }
+
+        // Pasamos el set a un Array para poderlo leer y manipular
+        Integer[] numerosFinales = numerosAleatorios.toArray(new Integer[0]);
+        // Creamos el List categoriaRandom que se generara de manera Random
+        List<Autor> autoresRandom = new ArrayList<>();
+
+        // Pasamos los valores a List<>
+        for (int i = 0; i < numerosFinales.length; i++) {
+            autoresRandom.add(autorServicio.obtenerAutorPorId(numerosFinales[i]).orElse(null));
+        }
+
+        // Mandando los libros a la vista
+        model.addAttribute("autores", autoresRandom);
+
         return model.toString();
     }
 }
