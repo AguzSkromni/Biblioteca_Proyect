7cd80fb5d175fd50c3813a839ec85a54a53aa87e
Agustin Reyes Hernandez <agustin.reyesh95@gmail.com>
2024-08-05 23:09:21 -0600

    Seccion Libros aleatorios y categorias aleatorias
    Se agregaron los metodos para desplegar de forma aleatoria en index, libros destacados y categorias destacadas asi como sus botones

--- ProBiblioteca/src/main/java/com/biblioteca/ProBiblioteca/controlador/IndexControlador.java
+++ ProBiblioteca/src/main/java/com/biblioteca/ProBiblioteca/controlador/IndexControlador.java
@@ -6,6 +6,12 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
+import com.biblioteca.ProBiblioteca.modelo.Autor;
+import com.biblioteca.ProBiblioteca.modelo.Categoria;
+import com.biblioteca.ProBiblioteca.modelo.Editorial;
+import com.biblioteca.ProBiblioteca.servicio.IAutorServicio;
+import com.biblioteca.ProBiblioteca.servicio.ICategoriaServicio;
+import com.biblioteca.ProBiblioteca.servicio.IEditorialServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,62 +27,110 @@ public class IndexControlador {
 
     @Autowired
     private ILibroServicio libroServicio;
-
-    public int contador = 0;
+    @Autowired
+    private IAutorServicio autorServicio;
+    @Autowired
+    private IEditorialServicio editorialServicio;
+    @Autowired
+    private ICategoriaServicio categoriaServicio;
 
     @GetMapping("/")
     public String mostrarIndex(Model model) {
+        randomLibroIndex(model);
+        categoriaIndex(model);
 
-        List<Libro> librosTodos = libroServicio.listarLibros();
-        //Recuperando el numero total de Libros
-        int totalLibros = librosTodos.size();
-        //variable Random
-        Random random = new Random();
-        //Genera un numero aleatorio entre el total de libros obtenidos
-        Set<Integer> numerosAleatorios = new HashSet<>();
-
-        //Crea 3 valores random en un SET,no permite que haya un valor igual
-        while (numerosAleatorios.size() < 3) {
-            int numeroAleatorios = random.nextInt(totalLibros) + 1;
-            numerosAleatorios.add(numeroAleatorios);
-        }
-
-        //Pasamos el set a un Array para poderlo leer y manipular
-        Integer[] numerosFinales = numerosAleatorios.toArray(new Integer[0]);
-        //Creamos el List libroDestacados que se generara de manera Random
-        List<Libro> librosDestacados = new ArrayList<>();
-
-        // Pasamos los valores a List<>
-        for(int i = 0; i < numerosFinales.length; i++){
-            librosDestacados.add(libroServicio.obtenerLibroPorId(numerosFinales[i]).orElse(null));
-        }
-
-        //Mandando los libros a la vista
-        model.addAttribute("libros", librosDestacados);
-        
-
-        return "index.xhtml";
+        return "index.xhtml"; // Retorna el nombre de la vista
     }
 
-
     @GetMapping("/autores")
-    public String listarAutores() {
+    public String listarAutores(Model model) {
+        // Obtén la lista de autores
+        List<Autor> listadoAutores = autorServicio.listarAutores();
+        // Añade la lista al modelo
+        model.addAttribute("autores", listadoAutores);
         return "autores.xhtml";
+
     }
 
     @GetMapping("/libros")
-    public String listarLibros() {
+    public String listarLibros(Model model) {
+        List<Libro> listadoLibros = libroServicio.listarLibros();
+        model.addAttribute("libros", listadoLibros);
         return "libros.xhtml";
     }
 
     @GetMapping("/editoriales")
-    public String listarEditoriales() {
+    public String listarEditoriales(Model model) {
+        List<Editorial> editorialList = editorialServicio.listaEditoriales();
+        model.addAttribute("editoriales", editorialList);
         return "editoriales.xhtml";
     }
 
     @GetMapping("/categorias")
-    public String listarCategorias() {
+    public String listarCategorias(Model model) {
+        List<Categoria> categoriaList = categoriaServicio.listaCategorias();
+        model.addAttribute("categorias", categoriaList);
         return "categorias.xhtml";
     }
 
+    /* METODOS INDEX */
+    public String randomLibroIndex(Model model){
+        List<Libro> librosTodos = libroServicio.listarLibros();
+        // Recuperando el numero total de Libros
+        int totalLibros = librosTodos.size();
+        // variable Random
+        Random random = new Random();
+        // Genera un numero aleatorio entre el total de libros obtenidos
+        Set<Integer> numerosAleatorios = new HashSet<>();
+
+        // Crea 3 valores random en un SET, no permite que haya un valor igual
+        while (numerosAleatorios.size() < 3) {
+            int numeroAleatorios = random.nextInt(totalLibros);
+            numerosAleatorios.add(numeroAleatorios);
+        }
+
+        // Pasamos el set a un Array para poderlo leer y manipular
+        Integer[] numerosFinales = numerosAleatorios.toArray(new Integer[0]);
+        // Creamos el List libroDestacados que se generara de manera Random
+        List<Libro> librosDestacados = new ArrayList<>();
+
+        // Pasamos los valores a List<>
+        for (int i = 0; i < numerosFinales.length; i++) {
+            librosDestacados.add(libroServicio.obtenerLibroPorId(numerosFinales[i]).orElse(null));
+        }
+
+        // Mandando los libros a la vista
+        model.addAttribute("libros", librosDestacados);
+        return model.toString();
+    }
+
+    public String categoriaIndex(Model model){
+        List<Categoria> categoriaTodos = categoriaServicio.listaCategorias();
+        // Recuperando el numero total de categorias
+        int totalCategorias = categoriaTodos.size();
+        // variable Random
+        Random random = new Random();
+        // Genera un numero aleatorio entre el total de categorias obtenidos
+        Set<Integer> numerosAleatorios = new HashSet<>();
+
+        // Crea 3 valores random en un SET, no permite que haya un valor igual
+        while (numerosAleatorios.size() < 5) {
+            int numeroAleatorios = random.nextInt(totalCategorias);
+            numerosAleatorios.add(numeroAleatorios);
+        }
+
+        // Pasamos el set a un Array para poderlo leer y manipular
+        Integer[] numerosFinales = numerosAleatorios.toArray(new Integer[0]);
+        // Creamos el List categoriaRandom que se generara de manera Random
+        List<Categoria> categoriaRandom = new ArrayList<>();
+
+        // Pasamos los valores a List<>
+        for (int i = 0; i < numerosFinales.length; i++) {
+            categoriaRandom.add(categoriaServicio.obtenerCategoriaPorId(numerosFinales[i]).orElse(null));
+        }
+
+        // Mandando los libros a la vista
+        model.addAttribute("categorias", categoriaRandom);
+        return model.toString();
+    }
 }
---
+++ ProBiblioteca/src/test/java/com/biblioteca/ProBiblioteca/servicio/IEditorialServicioTest.java
@@ -0,0 +1,7 @@
+package com.biblioteca.ProBiblioteca.servicio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IEditorialServicioTest {
+
+}
